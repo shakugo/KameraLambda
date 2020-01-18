a76632ce8991af90b42fe739719bd61c926fc575
tonchan <cleartone1216@gmail.com>
2020-01-19 00:58:31 +0900

    test post

--- .circleci/getResult.sh
+++ .circleci/getResult.sh
@@ -3,3 +3,4 @@
 ApiKeyID=`aws cloudformation describe-stacks --stack-name KameraLambda | jq -r '.Stacks[].Outputs | map(select(.OutputKey == "ApiKeyID")) | .[].OutputValue'`
 ApiGatewayEndpoint=`aws cloudformation describe-stacks --stack-name KameraLambda | jq -r '.Stacks[].Outputs | map(select(.OutputKey == "ApiGatewayEndpoint")) | .[].OutputValue'`
 ApiKey=`aws apigateway get-api-key --api-key $ApiKeyID --include-value | jq -r .value`
+echo $ApiKey
