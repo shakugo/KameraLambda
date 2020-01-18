6d5ad47871b277614ca4f9fa2008237b6921930c
tonchan <cleartone1216@gmail.com>
2020-01-19 01:34:02 +0900

    test post

--- .circleci/getResult.sh
+++ .circleci/getResult.sh
@@ -3,8 +3,7 @@
 ApiKeyID=`aws cloudformation describe-stacks --stack-name KameraLambda | jq -r '.Stacks[].Outputs | map(select(.OutputKey == "ApiKeyID")) | .[].OutputValue'`
 ApiGatewayID=`aws cloudformation describe-stacks --stack-name KameraLambda | jq -r '.Stacks[].Outputs | map(select(.OutputKey == "ApiGatewayID")) | .[].OutputValue'`
 ApiKey=`aws apigateway get-api-key --api-key $ApiKeyID --include-value | jq -r .value`
-echo $ApiKey
 
-curl -X POST --data-urlencode 'payload={"text": "API_BASE_URL=https://${ApiGatewayID}.execute-api.ap-northeast-1.amazonaws.com/develop
-\n API_KEY=${ApiKey}"}' $Slack_URL
+curl -X POST --data-urlencode "payload={\"text\": \"API_BASE_URL=https://${ApiGatewayID}.execute-api.ap-northeast-1.amazonaws.com/develop
+\n API_KEY=${ApiKey}\"}" $Slack_URL
 
