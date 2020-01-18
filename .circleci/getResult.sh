af5d28925c90109416c3d76b0ea6775a742007e4
tonchan <cleartone1216@gmail.com>
2020-01-19 01:13:13 +0900

    test post

--- .circleci/getResult.sh
+++ .circleci/getResult.sh
@@ -4,3 +4,5 @@ ApiKeyID=`aws cloudformation describe-stacks --stack-name KameraLambda | jq -r '
 ApiGatewayEndpoint=`aws cloudformation describe-stacks --stack-name KameraLambda | jq -r '.Stacks[].Outputs | map(select(.OutputKey == "ApiGatewayEndpoint")) | .[].OutputValue'`
 ApiKey=`aws apigateway get-api-key --api-key $ApiKeyID --include-value | jq -r .value`
 echo $ApiKey
+
+curl -X POST --data-urlencode "payload={\"text\": \"これは webhookbot という名の${ApiKey}から #aws-notice に投稿されています。\"}" $Slack_URL
