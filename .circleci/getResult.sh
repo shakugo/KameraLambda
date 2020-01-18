62ebf93d69f6dcd7322e97700d9b333b13e53088
tonchan <cleartone1216@gmail.com>
2020-01-19 01:22:43 +0900

    test post

--- .circleci/getResult.sh
+++ .circleci/getResult.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 ApiKeyID=`aws cloudformation describe-stacks --stack-name KameraLambda | jq -r '.Stacks[].Outputs | map(select(.OutputKey == "ApiKeyID")) | .[].OutputValue'`
-ApiGatewayEndpoint=`aws cloudformation describe-stacks --stack-name KameraLambda | jq -r '.Stacks[].Outputs | map(select(.OutputKey == "ApiGatewayEndpoint")) | .[].OutputValue'`
+ApiGatewayID=`aws cloudformation describe-stacks --stack-name KameraLambda | jq -r '.Stacks[].Outputs | map(select(.OutputKey == "ApiGatewayID")) | .[].OutputValue'`
 ApiKey=`aws apigateway get-api-key --api-key $ApiKeyID --include-value | jq -r .value`
 echo $ApiKey
 
-curl -X POST --data-urlencode "payload={\"text\": \"これは webhookbot という名の${ApiKey}から #aws-notice に投稿されています。\"}" $Slack_URL
+curl -X POST --data-urlencode 'payload={"text": "API_BASE_URL=https://${ApiGatewayID}.execute-api.ap-northeast-1.amazonaws.com/develop
+\n API_KEY=${ApiKey}"}' $Slack_URL
+
